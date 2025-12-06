4d78967bcf02576429ff5cb3791da7f37a40a8ce
Jb DOYON <jb@jiby.tech>
2025-12-06 14:26:24 +0000

    Add day 6 stub

---
+++ src/aoc2025/day6.py
@@ -0,0 +1,26 @@
+"""Day 6 solution to AoC 2025"""
+
+
+def solution1(puzzle_input) -> int:
+    """Solve day6 part 1"""
+    return 0
+
+
+def solution2(puzzle_input) -> int:
+    """Solve day6 part 2"""
+    return 0
+
+
+def read_puzzle_input(puzzle_input: str):
+    """Process the puzzle input string"""
+    return puzzle_input
+
+
+def solve1_string(puzzle_input: str) -> int:
+    """Convert list to proper format and solve day6 solution1"""
+    return solution1(read_puzzle_input(puzzle_input))
+
+
+def solve2_string(puzzle_input: str) -> int:
+    """Convert list to proper format and solve day6 solution2"""
+    return solution2(read_puzzle_input(puzzle_input))
